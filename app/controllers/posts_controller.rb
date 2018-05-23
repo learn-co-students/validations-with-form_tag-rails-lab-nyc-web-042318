1d0015b1ef03555d0bfc8c6e1a62d31f3ab56d15
Canino Developments <ian.canino@gmail.com>
2018-05-23 11:50:56 -0400

    missing the .require in the post_params method.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.permit(:title, :category, :content)
+    params.require(:post).permit(:title, :category, :content)
   end
 end
